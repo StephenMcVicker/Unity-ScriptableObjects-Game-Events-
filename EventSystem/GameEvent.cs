f2393746c2844b1b7df3fa4ea5c124648ad0c65e
NeoDragonCP <mcvickerstephen@gmail.com>
2018-07-20 11:25:24 +0100

    Add files via upload

--- EventSystem/GameEvent.cs
+++ EventSystem/GameEvent.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "Topply Knight/Game Event")]
+[CreateAssetMenu(menuName = "Game Event")]
 public class GameEvent : ScriptableObject
 {
 
